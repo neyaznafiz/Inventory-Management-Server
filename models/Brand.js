2f8aa156302b0acff1dbf2d412a23d1a488ee061
neyaznafiz <niiyazobug@gmail.com>
2022-11-13 16:54:01 +0600

    category schema and model created

--- models/Brand.js
+++ models/Brand.js
@@ -2,59 +2,59 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const { ObjectId } = mongoose.Schema.Types;
 
-const brandSchema = mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    require: [true, "Please provide a brand name..!"],
-    maxLength: 100,
-    unique: true,
-    lowercase: true,
-  },
+const brandSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      require: [true, "Please provide a brand name..!"],
+      maxLength: 100,
+      unique: true,
+      lowercase: true,
+    },
 
-  description: String,
+    description: String,
 
-  location: String,
+    location: String,
 
-  email: {
-    type: String,
-    lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email..!"],
-  },
+    email: {
+      type: String,
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email..!"],
+    },
 
-  website: {
-    type: String,
-    validate: [validator.isURL, "Please provide a valid URL..!"],
-  },
+    website: {
+      type: String,
+      validate: [validator.isURL, "Please provide a valid URL..!"],
+    },
 
-  products: [
-    {
-      type: ObjectId,
-      ref: "Product",
-    }
-  ],
-
-  suppliers: [
-    {
-      name: String,
-      contactNumber: String,
-      id: {
+    products: [
+      {
         type: ObjectId,
-        ref: "Supplier",
+        ref: "Product",
       }
-    }
-  ],
+    ],
 
-  status: {
-    type: String,
-    enum: ["active", "inactive"],
-    default: "active",
+    suppliers: [
+      {
+        name: String,
+        contactNumber: String,
+        id: {
+          type: ObjectId,
+          ref: "Supplier",
+        }
+      }
+    ],
+
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
   },
+  { timestamps: true }
+);
 
-  timestamps: true,
-});
+const Brand = mongoose.model("Brand", brandSchema);
 
-
-const Brand = mongoose.Model('Brand', brandSchema)
-
-exports = Brand
+exports = Brand;
